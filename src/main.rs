9f17f508a01665e94fc6cdddc6ffa21d7d06f685
TomaszJaworski777 <zir3xx@gmail.com>
2024-07-24 22:29:31 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,4 @@ use spear::{ChessBoard, Move, MoveFlag, Square, FEN};
 fn main() {
     let mut board = ChessBoard::from_fen(&FEN::kiwipete_position());
     board.draw_board();
-
-    let mv = Move::from_squares( Square::from_string("g2"), Square::from_string("g4"), MoveFlag::DOUBLE_PUSH );
-    board.make_move(mv);
-    board.draw_board();
-
-    let mv = Move::from_squares( Square::from_string("h8"), Square::from_string("g8"), MoveFlag::QUIET_MOVE );
-    board.make_move(mv);
-    board.draw_board();
-
-    let mv = Move::from_squares( Square::from_string("e1"), Square::from_string("g1"), MoveFlag::KING_SIDE_CASTLE );
-    board.make_move(mv);
-    board.draw_board();
 }
